374beafca005ad5b4cd9b705420e4ced13961e78
Nichita <nichita.asterocode@gmail.com>
2026-06-08 19:00:23 +0300

    Design system: Add design system tokens and global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>Naologic Schedule</h1>`,
+  template: `<h1 class="nao-title" style="padding:40px">Naologic Schedule</h1>`,
 })
 export class AppComponent {}
